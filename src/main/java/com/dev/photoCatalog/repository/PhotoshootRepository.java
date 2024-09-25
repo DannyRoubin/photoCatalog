db3ab1ed858fa2ca9631ea4fb5bafc610788090c
Danny Roubin <danny.roubin@avanade.com>
2024-09-25 10:05:05 -0700

    created the service and repository layer
    Signed-off-by: Danny Roubin <danny.roubin@avanade.com>

---
+++ src/main/java/com/dev/photoCatalog/repository/PhotoshootRepository.java
@@ -0,0 +1,5 @@
+package com.dev.photoCatalog.repository;
+
+public class PhotoshootRepository {
+    
+}
---
+++ src/main/java/com/dev/photoCatalog/service/LocationService.java
@@ -0,0 +1,5 @@
+package com.dev.photoCatalog.service;
+
+public class LocationService {
+    
+}
